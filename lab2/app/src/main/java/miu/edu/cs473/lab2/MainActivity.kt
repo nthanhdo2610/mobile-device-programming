1d97419ca7c3e43acc3fa57f8161ddec0e8145b4
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-10-29 15:43:32 -0700

    lab 2

--- lab2/app/src/main/java/miu/edu/cs473/lab2/MainActivity.kt
+++ lab2/app/src/main/java/miu/edu/cs473/lab2/MainActivity.kt
@@ -1,11 +1,43 @@
 package miu.edu.cs473.lab2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import miu.edu.cs473.lab2.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var menus: MutableList<String> = mutableListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initView()
     }
+
+    private fun initView() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvFood.text = menus[0]
+    }
+
+    fun onClickAddFood(view: View) {
+        val newFood = binding.etFood.text.toString().trim()
+        if (newFood.isEmpty()) {
+            Toast.makeText(this, "Please input a new food", Toast.LENGTH_SHORT).show()
+            return
+        }
+        menus.add(newFood)
+        binding.etFood.text.clear()
+        binding.tvFood.text = newFood
+    }
+
+    fun onDecide(view: View) {
+        val random = Random.nextInt(menus.size)
+        binding.tvFood.text = menus[random]
+    }
+
 }
