c0ab28492bfb54bac034f7e14eca520b64aa4268
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-11-06 11:48:16 -0800

    lab 4

--- Lab4/app/src/main/java/miu/edu/cs473/lab4/MainActivity.kt
+++ Lab4/app/src/main/java/miu/edu/cs473/lab4/MainActivity.kt
@@ -2,10 +2,23 @@ package miu.edu.cs473.lab4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import miu.edu.cs473.lab4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onSignIn(view: View) {
+
+    }
+
+    fun onSignUp(view: View) {
+
     }
 }
