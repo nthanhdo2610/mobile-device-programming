2013dec776a256b4a2811b41ef53a8e7eb87d081
nthanhdo2610 <nthanhdo11110030@gmail.com>
2024-01-24 11:15:26 -0800

    practicing

--- Sunriseset/app/src/main/java/com/bright/sunriseset/AccessCameraGalleryActivity.kt
+++ Sunriseset/app/src/main/java/com/bright/sunriseset/AccessCameraGalleryActivity.kt
@@ -4,14 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
+import com.bright.sunriseset.databinding.ActivityAccessCameraGalleryBinding
 
 class AccessCameraGalleryActivity : AppCompatActivity() {
 
-    lateinit var startForResultCamera : ActivityResultLauncher<Intent>
-    lateinit var startForResultGalley : ActivityResultLauncher<Intent>
+    private lateinit var binding: ActivityAccessCameraGalleryBinding
+
+    private lateinit var startForResultCamera: ActivityResultLauncher<Intent>
+    private lateinit var startForResultGalley: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_access_camera_gallery)
+        binding = ActivityAccessCameraGalleryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ Sunriseset/app/src/main/java/com/bright/sunriseset/VideoRecordingActivity.kt
@@ -0,0 +1,67 @@
+package com.bright.sunriseset
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bright.sunriseset.databinding.ActivityVideoRecordingBinding
+
+class VideoRecordingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVideoRecordingBinding
+    private val captureCode = 101
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inflate the layout using View Binding
+        binding = ActivityVideoRecordingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recordButton.isEnabled = hasCamera()
+    }
+
+    private fun hasCamera(): Boolean {
+        return packageManager.hasSystemFeature(
+            PackageManager.FEATURE_CAMERA_ANY
+        )
+    }
+
+    fun startRecording(view: View) {
+        val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+        startActivityForResult(intent, captureCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val videoUri = data?.data
+        if (requestCode == captureCode) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    Toast.makeText(
+                        this, "Video saved to:\n"
+                                + videoUri, Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                Activity.RESULT_CANCELED -> {
+                    Toast.makeText(
+                        this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                else -> {
+                    Toast.makeText(
+                        this, "Failed to record video",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+}
